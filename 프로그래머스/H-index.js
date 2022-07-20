ebfae0a51d66650f6ed2bc555d75c9b2d664f191
SangHyun <duddjshfl@naver.com>
2022-07-20 22:54:47 +0900

    프로그래머스 update

--- 프로그래머스/H-index.js
+++ 프로그래머스/H-index.js
@@ -1,17 +1,19 @@
+// 1차 시도 테스트케이스 1~10, 12~15 실패
+// 2차 시도 성공
+
+// 틀린 이유
+// 역순으로 정렬 -> for문으로 순회하며 i+1 >= citations[i]로 뒀는데
+// i+1 <= citations[i]가 맞는 거였다
+
 const citations = [3, 0, 6, 1, 5]
 
 function solution(citations){
     let answer = 0
 
-    citations.sort((a, b) => {return b - a})
+    citations.sort((a, b)=>{return b - a})
 
-    for(let i = 0; i < citations.length; i++){
-        if(citations[i] <= i + 1 && citations[i] >= citations.length - (i + 1)){
-            // citations[i]보다 더 인용된 논문수 = index + 1
-            // citations[i]보다 덜 인용된 논문 수 = citations.length - (index + 1)
-            answer = citations[i]
-            break
-        }
+    for(let i = 0; i+1 <=citations[i]; i++){
+        answer = i + 1
     }
 
     return answer
--- 프로그래머스/배달.js
+++ 프로그래머스/배달.js
@@ -1,7 +1,49 @@
+// 1차 시도
 // 테스트케이스 6, 12~29 실패
+
+// 2차 시도
+// 테스트케이스 6, 11~13, 15~17, 20~28, 32 실패
 
 const N = 5, K = 3
 const road = [[1,2,1],[2,3,3],[5,2,2],[1,4,2],[5,3,1],[5,4,2]]
+
+function solution2(N, road, K){
+    let answer = 0
+    // 최단거리
+    let far = []
+    let nextVisit = 1
+    let visited = [1]
+
+    for(let i = 0; i <= N; i++){
+        far[i] = Infinity
+    }
+    far[1] = 0
+
+    while(nextVisit){
+        let checkRoad = road.filter(e => e[0] === nextVisit || e[1] === nextVisit).sort((a, b) => a[2] - b[2])
+        console.log(nextVisit)
+        console.log(checkRoad)
+        checkRoad.forEach(e =>{
+            const [from, to, length] = e
+            if(from === nextVisit){
+                far[to] = far[from] + length < far[to] ? far[from] + length : far[to]
+            }else{
+                far[from] = far[to] + length < far[from] ? far[to] + length : far[from]
+            }
+        })
+        
+        let candidate = checkRoad.filter(e => !visited.includes(e[0]) || !visited.includes(e[1]))
+        // console.log(nextVisit + '\n' + candidate)
+        candidate = candidate.map(e => e[0] === nextVisit ? e[1] : e[0]).filter(e => !visited.includes(e))
+        nextVisit = candidate[0]
+        visited.push(nextVisit)
+    }
+
+    far.shift()
+    console.log(far)
+
+    return far.filter(e => e <= K).length
+}
 
 function solution(N, road, K) {
     let answer = 0;
@@ -45,5 +87,5 @@ function solution(N, road, K) {
     return answer;
 }
 
-console.log(solution(N, road, K))
+console.log(solution2(N, road, K))
 
