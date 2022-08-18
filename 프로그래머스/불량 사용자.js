cc1ecb5227f3c11c40aab6b3ada4883a445b71d7
SangHyun <duddjshfl@naver.com>
2022-08-18 23:58:27 +0900

    프로그래머스 update

--- 프로그래머스/불량 사용자.js
+++ 프로그래머스/불량 사용자.js
@@ -1,8 +1,10 @@
-// 미완
+// 1차 시도 실패
+// 테스트케이스 6, 7, 9, 11 실패, 5 시간초과
 
 function solution(user_id, banned_id) {
     let answer = 0;
     let availId = []
+    let combinations = []
     const idList = {}
     
     for(let i = 0; i < banned_id.length; i++){
@@ -12,12 +14,28 @@ function solution(user_id, banned_id) {
     
     for(let i = 0; i < availId.length; i++){
         user_id.forEach(e =>{
-            if(e.match(new RegExp(availId[i], 'g'))){
+            if(e.match(new RegExp(availId[i] + '$', 'g'))){
                 idList[i] ? idList[i].push(e) : idList[i] = [e]
             }
         })
     }
-    console.log(idList)
     
-    return answer;
+    for(const key in idList){
+        if(!combinations.length){
+            combinations = [...idList[key]]
+        }else{
+            let nextCombinations = []
+            combinations.forEach(e =>{
+                idList[key].forEach(x => {
+                    nextCombinations.push(`${e} ${x}`)
+                })
+            })
+            combinations = [...nextCombinations]
+        }
+    }
+    
+    combinations = combinations.map(e => e.split(' ').sort().join(' '))
+        .filter(e => new Set(e.split(' ')).size === availId.length)
+    
+    return new Set(combinations).size;
 }
