1d1481b7089efa4bc11543df6dec7474669bada4
SangHyun <duddjshfl@naver.com>
2022-08-30 21:46:02 +0900

    프로그래머스 update

--- 프로그래머스/불량 사용자.js
+++ 프로그래머스/불량 사용자.js
@@ -3,6 +3,42 @@
 
 // 5번 시간초과 해결
 
+// 2차 시도 실패
+// 테스트케이스 2, 6, 7, 9, 10, 11 실패
+// 아마 'abcd'.includes('abc')가 true가 나와서 생기는 문제 같다 (28번째 줄)
+
+// 2차 코드
+function solution(user_id, banned_id) {
+    let ids = []
+    banned_id = banned_id.map(e => e.replace(/\*/g, '.') + '$')
+    
+    for(let i = 0; i < banned_id.length; i++){
+        if(ids.length === 0){
+            user_id.forEach(e =>{
+                if(e.match(new RegExp(banned_id[i]))){
+                    ids.push(e)
+                }
+            })
+            continue
+        }
+        
+        let temp = []
+        ids.forEach(e =>{
+            user_id.forEach(v => {
+                if(v.match(new RegExp(banned_id[i])) && !e.includes(v)){
+                    temp.push(`${e} ${v}`)
+                }
+            })
+        })
+        ids = [...temp]
+    }
+    
+    ids = ids.filter(e => new Set(e.split(' ')).size === banned_id.length)
+            .map(e => e.split(' ').sort().join(' '))
+                 
+    return new Set(ids).size
+}
+
 function solution(user_id, banned_id) {
     let availId = []
     let combinations = []
