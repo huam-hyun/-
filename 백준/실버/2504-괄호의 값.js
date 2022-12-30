9ce6d69d74ecf04a43a7393f4217cbd5aa7419b1
SangHyun <duddjshfl@naver.com>
2022-12-30 23:56:24 +0900

    백준 실버 Update

--- 백준/실버/2504-괄호의 값.js
+++ 백준/실버/2504-괄호의 값.js
@@ -1,30 +1,36 @@
-const input = '())([]';
+const input = '()()[]';
 const stack = [];
-let answer = ''
+let answer = 0;
+let temp = 1;
 
 for(let i = 0; i < input.length; i++){
     const cur = input[i];
 
-    if(cur === '(' || cur === '['){
+    if(cur === '('){
         stack.push(cur);
-        answer += cur === '(' ? '+2*(' : '+3*('
-    }else{
-        if(stack[stack.length - 1] === '(' && cur === ')'){
-            stack.pop();
-            answer += ')';
-        }else if(stack[stack.length - 1] === '[' && cur === ']'){
-            stack.pop();
-            answer += ')';
-        }else if(cur === ')' || cur === ']'){
-            stack.push(cur);
+        temp *= 2;
+    }else if(cur === '['){
+        stack.push(cur);
+        temp *= 3;
+    }else if(cur === ')'){
+        if(stack.length === 0 || stack[stack.length - 1] === '['){
+            answer = 0;
+            break;
+        }else if(stack[stack.length - 1] === '('){
+            answer += temp;
         }
+        stack.pop();
+        temp = Math.floor(temp / 2);
+    }else{
+        if(stack.length === 0 || stack[stack.length - 1] === '('){
+            answer = 0;
+            break;
+        }else if(stack[stack.length - 1] === '['){
+            answer += temp;
+        }
+        stack.pop();
+        temp = Math.floor(temp / 3);
     }
 }
 
-if(stack.length) answer = '0';
-
-while(answer.includes('()')){
-    answer = answer.replace('()', '1');
-}
-
-console.log(eval(answer));
+console.log(answer);
