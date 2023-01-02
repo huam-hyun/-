1109856a060192540e2ceeb27deeb2d6a128ec7d
SangHyun <duddjshfl@naver.com>
2023-01-02 17:58:34 +0900

    LeetCode Medium Update

--- LeetCode/Medium/Generate Parentheses.js
+++ LeetCode/Medium/Generate Parentheses.js
@@ -4,6 +4,28 @@
 // checkParenthesis 함수로 어떤 괄호를 추가할 수 있는지 체크 후 nextSet에 저장
 // nextSet이 없을 때까지 반복한다
 
+// 성공2
+// Beats 66.92% / 99.27%
+// dfs를 통해 더 효율적으로 답을 찾을 수 있다
+
+var generateParenthesis = function(n) {
+    if(n === 1) return ['()'];
+
+    const answer = [];
+
+    dfs('', 0, 0);
+
+    return answer;
+
+    function dfs(string, left, right){
+        // answer
+        if(string.length === 2 * n) answer.push(string);
+        
+        if(left < n) dfs(string + '(', left + 1, right);
+        if(right < left) dfs(string + ')', left, right + 1);
+    }
+};
+
 /**
  * @param {number} n
  * @return {string[]}
