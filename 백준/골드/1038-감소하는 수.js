f4945911a6823cffda38c86d7bdc7113290895cd
SangHyun <duddjshfl@naver.com>
2024-02-09 21:25:49 +0900

    백준 update

--- 백준/골드/1038-감소하는 수.js
+++ 백준/골드/1038-감소하는 수.js
@@ -1,46 +1,54 @@
 // const fs = require('fs')
 // const input = fs.readFileSync("/dev/stdin").toString().trim().split('\n')
+// 현재 조건을 만족하는 숫자를 만드는 함수
 const makeNumber = (tensDigit, num, remain, dp) => {
   if (remain === 0) {
+    // num 으로 시작하는 숫자의 가장 첫 숫자를 반환한다
+    // 한 자리 수 = x
+    // 여러 자리 수 = x____ -> 0에서 남은 자릿수 거꾸로 해서 붙인거
     return tensDigit === 0
       ? String(num)
       : String(num) + Array.from({ length: tensDigit }, (_, i) => i).reverse().join('')
   }
   const nums = dp[tensDigit][num]
   return nums > remain
-    ? String(num) + makeNumber(tensDigit - 1, 0, remain, dp) // 다음 단위로
-    : makeNumber(tensDigit, num + 1, remain - nums, dp) // 다음 숫자로
+    ? String(num) + makeNumber(tensDigit - 1, 0, remain, dp) // 지금 조건에서 맞는 수가 있다 -> 현재 숫자를 문자열에 포함하고 다음 단위 0부터
+    : makeNumber(tensDigit, num + 1, remain - nums, dp)      // 지금 조건에서 맞는 수가 없다 -> 다음 숫자로
 }
 const inputs = Array.from({ length: 1023 }, (_, i) => i)
 
 const solution = (input) => {
+  // 한 자릿 수 제외
   if (input < 10) {
     return input
   }
-  // 한 자리 수일 경우는 제외했으니 input에서도 빼줌
+  // 한 자릿 수일 경우는 제외했으니, input에서도 빼줌
   input -= 10
+  // 숫자 길이, 시작 숫자로 가능한 조합을 저장할 dp
   const dp = Array.from({ length: 10 }, () => [])
   dp[0] = new Array(10).fill(1)
-  // 몇 자리 수
+  // 자릿수
   let tensDigit = 1
   // 시작 숫자
   let num = 0
 
   while (input >= 0) {
-    // num으로 시작했을 때 tensDigit 자리수가 가질 수 있는 숫자의 갯수
-    // 0으로 시작할 경우 한자리 수일때만 1이고 나머지는 0
+    // tensDigit 길이, num으로 시작하는 숫자가 가능한 갯수
+    // 0으로 시작할 경우 한자릿 수일때만 1이고 나머지는 0
     const nums = dp[tensDigit][num] = num === 0
       ? dp[tensDigit][num] || 0
       : dp[tensDigit][num - 1] + dp[tensDigit - 1][num - 1]
 
-    // 현재 가능한 숫자의 갯수가 정답까지 남은 갯수보다 많으면
-    // num으로 시작하는 현재 자리수에 정답이 있음
+    // 현재 가능한 숫자의 개수가 정답까지 남은 개수보다 많으면
+    // num으로 시작하는 현재 자릿수에 정답이 있음
+    // 정답 === num으로 시작하는 testDigit 길이의 문자 중 남은 input 번째 숫자
     if (nums > input) {
       return makeNumber(tensDigit, num, input, dp)
     }
-    // 남은 갯수에서 현재 가능한 갯수를 뺀다
+    // 남은 input의 개수가 현재 가능한 조합보다 많으면
+    // 남은 개수에서 현재 가능한 개수를 뺀다
     input -= nums
-    // 9로 시작할 경우 다음에 시작할 숫자는 0이고 자리수를 하나 증가시킨다
+    // 9로 시작할 경우 다음에 시작할 숫자는 0이고 자릿수를 하나 증가시킨다
     // 아닐 경우 시작할 숫자만 증가시킨다
     if (num === 9) {
       num = 0
--- 백준/실버/1051-숫자 정사각형.js
+++ 백준/실버/1051-숫자 정사각형.js
@@ -23,12 +23,15 @@ const solution = (input) => {
   input = input.split('\n')
   const [N, M] = input.shift().split(' ').map(Number)
   let max = 0
-  
+  // 현재 가장 큰 사각형보다 작을때는 비교할 필요가 없으니 (사각형 크기 - max) 까지만
   for(let i = 0; i < N - max; i++) {
     for(let j = 0; j < M - max; j++) {
+      // 현재 위치의 숫자
       const num = input[i][j]
-      
+      // max보다 클때만 보면 되니까 max + 1부터 확인
+      // 현재 위치에서 확인할 길이(k)가 사각형을 벗어나지 않는 범위에서 길이(k) 증가시키면서 확인
       for(let k = max + 1; i + k < N && j + k < M; k++) {
+        // 각 꼭짓점이 현재 위치의 숫자와 같다면 max를 현재 길이(k)로 변경
         if ([input[i + k][j], input[i][j + k], input[i + k][j + k]]
           .every((value) => value === num)
         ) {
