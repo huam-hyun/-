c9102bfce2b19f2e1d813b3342dcd8543a45a441
Sanghyun <80454973+huam-hyun@users.noreply.github.com>
2024-02-11 21:30:36 +0900

    [level 0] Title: 주사위 게임 2, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.js
@@ -0,0 +1,10 @@
+function solution(a, b, c) {
+    let answer = a + b + c
+    if (a === b || b === c || a === c) {
+        answer *= (a ** 2 + b ** 2 + c ** 2)
+    }
+    if (a === b && b === c) {
+        answer *= (a ** 3 + b ** 3 + c ** 3)
+    }
+    return answer
+}
