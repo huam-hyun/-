073702000df4e84a3bc9aeb7ef9f3da172ff2605
Sanghyun <80454973+huam-hyun@users.noreply.github.com>
2024-03-02 14:57:12 +0900

    [level 3] Title: 불량 사용자, Time: 0.74 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/3/64064. 불량 사용자/불량 사용자.js
+++ 프로그래머스/3/64064. 불량 사용자/불량 사용자.js
@@ -1,35 +1,19 @@
 function solution(user_id, banned_id) {
-    const ids = new Set();
-    let used = new Array(banned_id.length).fill(false);
-    
-    banned_id = banned_id.map(e => '^' + e.replace(/\*/g, '.') + '$')
-    let needVisit = user_id.filter(e => e.match(banned_id[0]));
-    
-    for(let i = 0; i < needVisit.length; i++){
-        dfs(needVisit[i], [], [...used], user_id, banned_id, [], ids);
-    }
-    
-    return new Set(ids).size;
-}
+    const bannedRegExp = banned_id.map(e => '^' + e.replace(/\*/g, '.') + '$')
+    const matchedIds = bannedRegExp.map((regExp) => user_id.filter((id) => id.match(regExp)))
+    const combinations = new Set(
+        matchedIds
+            .reduce((p, c) => {
+                const newCombination = []
+                c.forEach((id) => {
+                  p.filter((set) => !set.includes(id)).forEach((set) => {
+                      newCombination.push([...set, id])
+                  })
+                })
+                return newCombination
+            }, [[]])
+            .map((set) => set.sort().join(' '))
+    )
 
-function dfs(check, checked, used, user_id, banned_id, array, set){  
-    const id = check;
-    const index = banned_id.findIndex((ban, i) => id.match(ban) && !used[i]);
-    if(index >= 0 && !checked.includes(id)){
-        used[index] = true;
-        checked.push(id);
-        
-        array[index] = id;
-        
-        if(used.includes(false)){
-            const next = banned_id[used.indexOf(false)];
-            const nextIds = user_id.filter(e => e.match(next) && !checked.includes(e));
-            
-            for(let i = 0; i < nextIds.length; i++){
-                dfs(nextIds[i], [...checked], [...used], user_id, banned_id, [...array], set);
-            }
-        }else{
-            set.add(array.sort().join(' '));
-        }
-    }
+    return combinations.size
 }
